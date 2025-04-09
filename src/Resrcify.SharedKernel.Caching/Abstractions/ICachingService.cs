abfbde398314ddef93d7b8b439487f5fc78082ac
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2025-04-09 19:14:16 +0200

    Support for jsonoptions in caching

--- src/Resrcify.SharedKernel.Caching/Abstractions/ICachingService.cs
+++ src/Resrcify.SharedKernel.Caching/Abstractions/ICachingService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,8 +8,23 @@ namespace Resrcify.SharedKernel.Caching.Abstractions;
 
 public interface ICachingService
 {
-    Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default) where T : class;
-    Task SetAsync<T>(string key, T value, TimeSpan expiration, CancellationToken cancellationToken = default) where T : class;
-    Task RemoveAsync(string key, CancellationToken cancellationToken = default);
-    Task<IEnumerable<T?>> GetBulkAsync<T>(IEnumerable<string> keys, CancellationToken cancellationToken = default);
+    Task<T?> GetAsync<T>(
+        string key,
+        JsonSerializerOptions? options = null,
+        CancellationToken cancellationToken = default)
+        where T : class;
+    Task SetAsync<T>(
+        string key,
+        T value,
+        TimeSpan expiration,
+        JsonSerializerOptions? options = null,
+        CancellationToken cancellationToken = default)
+        where T : class;
+    Task RemoveAsync(
+        string key,
+        CancellationToken cancellationToken = default);
+    Task<IEnumerable<T?>> GetBulkAsync<T>(
+        IEnumerable<string> keys,
+        JsonSerializerOptions? options = null,
+        CancellationToken cancellationToken = default);
 }
--- src/Resrcify.SharedKernel.Caching/Primitives/InMemoryCachingService.cs
+++ src/Resrcify.SharedKernel.Caching/Primitives/InMemoryCachingService.cs
@@ -20,43 +20,51 @@ public sealed class InMemoryCachingService : ICachingService
 
     public async Task<T?> GetAsync<T>(
         string key,
+        JsonSerializerOptions? options = null,
         CancellationToken cancellationToken = default)
         where T : class
     {
         byte[]? cachedValue = await _distributedCache.GetAsync(key, cancellationToken);
         if (cachedValue is null)
             return null;
-        return JsonSerializer.Deserialize<T>(cachedValue);
+        return JsonSerializer.Deserialize<T>(cachedValue, options);
     }
 
     public async Task SetAsync<T>(
         string key,
         T value,
         TimeSpan expiration,
+        JsonSerializerOptions? options = null,
         CancellationToken cancellationToken = default)
         where T : class
     {
-        byte[] cachedValue = Serialize(value);
-        var options = new DistributedCacheEntryOptions()
+        byte[] cachedValue = Serialize(value, options);
+
+        var cacheEntryOptions = new DistributedCacheEntryOptions()
             .SetSlidingExpiration(expiration);
-        await _distributedCache.SetAsync(key, cachedValue, options, cancellationToken);
+        await _distributedCache.SetAsync(key, cachedValue, cacheEntryOptions, cancellationToken);
     }
-    private static byte[] Serialize<T>(T value)
+    private static byte[] Serialize<T>(T value, JsonSerializerOptions? options)
     {
         var buffer = new ArrayBufferWriter<byte>();
         using var writer = new Utf8JsonWriter(buffer);
-        JsonSerializer.Serialize(writer, value);
+        JsonSerializer.Serialize(writer, value, options);
         return buffer.WrittenSpan.ToArray();
     }
 
-    public async Task RemoveAsync(string key, CancellationToken cancellationToken = default)
+    public async Task RemoveAsync(
+        string key,
+        CancellationToken cancellationToken = default)
         => await _distributedCache.RemoveAsync(key, cancellationToken);
 
-    public async Task<IEnumerable<T?>> GetBulkAsync<T>(IEnumerable<string> keys, CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<T?>> GetBulkAsync<T>(
+        IEnumerable<string> keys,
+        JsonSerializerOptions? options = null,
+        CancellationToken cancellationToken = default)
     {
         var tasks = keys.Select(key => _distributedCache.GetAsync(key, cancellationToken)).ToList();
         var cachedValues = await Task.WhenAll(tasks);
         return cachedValues
-            .Select(bytes => JsonSerializer.Deserialize<T>(bytes));
+            .Select(bytes => JsonSerializer.Deserialize<T>(bytes, options));
     }
 }
